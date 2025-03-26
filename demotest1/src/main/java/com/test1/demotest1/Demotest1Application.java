ab26cb077403138a78212a25ee88d66d1994b928
stambala <stambalanagaraju99@gmail.com>
2025-03-26 04:05:49 -0700

    aading new service

---
+++ demotest1/src/main/java/com/test1/demotest1/Demotest1Application.java
@@ -0,0 +1,13 @@
+package com.test1.demotest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demotest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demotest1Application.class, args);
+	}
+
+}
---
+++ demotest1/src/test/java/com/test1/demotest1/Demotest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test1.demotest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demotest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
